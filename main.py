63838775fc22fbe87f522a7bfdda48bd5c56c979
Javaboiiii <devrajlokhande1610@gmail.com>
2025-10-22 14:45:44 +0530

    Adding RAG of Pinecone as well as on top to look response clean added ai response

--- main.py
+++ main.py
@@ -5,35 +5,67 @@ import uvicorn
 import json 
 from pydantic import BaseModel
 from vector_db import pc 
+from google import genai
 
 app = FastAPI() 
 
+index = pc.Index(os.getenv('INDEX_NAME'))
+
+client = genai.Client()
 
 @app.route('/embed_data', methods=['POST'])
 async def insert_data(req: Request): 
-    # try:
+    try:
         body = await req.json()
-        index = pc.Index(os.getenv('INDEX_NAME'))
         index.upsert(namespace=body['namespace'], vectors=[body['vector']]) 
         res = {
             "message": "Data embeded successfully"
         }
 
         return Response(json.dumps(res), status_code=200) 
-    # except: 
-
-    #     res = {
-    #         "message": "Failed to embed data"
-    #     }
-    #     return Response(json.dumps(res), status_code=500) 
+    except:
+        response = {
+            "message": "Unable to embedd data. May be some field is missing"
+        }
+        return Response(json.dump(response), status_code=400)
 
 @app.route("/report", methods=['GET'])
 async def getReport(req: Request) : 
-    data = await req.json()
+    try:
+        data = await req.json()
 
-    return Response(content=json.dumps(data), status_code=200, headers={
-        "Content-Type": "application/json"
-    })
+        print(data['namespace'])
+
+        result = index.search(
+            namespace=data['namespace'], 
+            query={
+                "top_k": 5,
+                "inputs":{
+                    "text": data['query'] 
+                }
+
+            }
+        )
+
+        ai_res = client.models.generate_content(
+            model="gemini-2.5-flash", contents=f"Give me neat, short response on my vector database result. Below is the query ${data['query']} by user ${data['namespace']} and below is result from my pinecone database ${result}. Now you need to do is give a neat result based on query and result to the user. Also don't let him know the we are using the database and ai is generating this result. I want neat and clean humanise answer"
+        )
+
+        response = {
+            "result": ai_res.text
+        }
+
+        return Response(json.dumps(response), status_code=200, headers={
+            "Content-Type": "application/json"
+        })
+    except: 
+        response = {
+            "message": "Unable to query. Try again"
+        }
+        return Response(json.dumps(response), status_code=400, headers={
+            "Content-Type": "application/json"
+        })
+
 
 
 if __name__ == "__main__" : 
